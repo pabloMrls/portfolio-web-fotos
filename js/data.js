0c82457c3ead7bfe1edd91e1d3ae7938a16521cb
Pabloc <pabloc12@gmail.com>
2026-02-01 18:51:22 -0300

    style: creé nombre de variables de stilos

--- js/data.js
+++ js/data.js
@@ -3,20 +3,20 @@ export const fotos = [
   {
     id: 1,
     titulo: "Vista aerea",
-    categoria: "paisaje",
+    categoria: "Paisaje",
     src: "/img/deportes/deporte.jpg",
     destacada: false
   },
   {
     id: 2,
     titulo: "Primer beso",
-    categoria: "bodas",
+    categoria: "Bodas",
     src: "/img/bodas/bodas.jpeg"
   },
   {
     id: 3,
     titulo: "Felicidad",
-    categoria: "bodas",
+    categoria: "Bodas",
     src: "https://picsum.photos/id/1013/600/400",
     destacada: false
   },
@@ -25,14 +25,14 @@ export const fotos = [
   {
     id: 4,
     titulo: "Salida",
-    categoria: "runner",
+    categoria: "Runner",
     src: "/img/runner/running.jpg",
     destacada: true
   },
   {
     id: 5,
     titulo: "Ritmo",
-    categoria: "runner",
+    categoria: "Runner",
     src: "/img/runner/running (2).jpg",
     destacada: true
   },
@@ -47,33 +47,33 @@ export const fotos = [
   {
     id: 7,
     titulo: "Tenis",
-    categoria: "deporte",
+    categoria: "Deporte",
     src: "/img/deportes/tenis.jpg"
   },
   {
     id: 8,
     titulo: "Fuerza",
-    categoria: "deporte",
+    categoria: "Deporte",
     src: "/img/deportes/deporte (2).jpg",
     destacada: true
   },
   {
     id: 9,
     titulo: "Caminata",
-    categoria: "deporte",
+    categoria: "Deporte",
     src: "/img/deportes/deporte (5).jpg",
     destacada: false
   },
   {
     id: 10,
     titulo: "Tenis",
-    categoria: "deporte",
+    categoria: "Deporte",
     src: "/img/deportes/deporte (4).jpg"
   },
    {
     id: 11,
     titulo: "Destreza",
-    categoria: "deporte",
+    categoria: "Deporte",
     src: "/img/deportes/deporte (3).jpg"
   },
 
