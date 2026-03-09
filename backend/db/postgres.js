b3f65997dc8742182219b7455b1203b3febfc1e1
Pabloc <pabloc12@gmail.com>
2026-03-08 22:48:39 -0300

    fix duplicate Pool import

--- backend/db/postgres.js
+++ backend/db/postgres.js
@@ -3,24 +3,6 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-// export const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-
-//   ssl: process.env.NODE_ENV === "production"
-//     ? { rejectUnauthorized: false }
-//     : false
-// });
-
-
-// pool.on("connect", () => {
-//   console.log("PostgreSQL conectado");
-// });
-
-// pool.on("error", (err) => {
-//   console.error("Error inesperado en PostgreSQL", err);
-// });
-
-
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
@@ -35,3 +17,20 @@ pool.on("connect", () => {
 pool.on("error", (err) => {
   console.error("Error inesperado en PostgreSQL", err);
 });
+
+// export const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+
+//   ssl: process.env.NODE_ENV === "production"
+//    d ? { rejectUnauthorized: false }
+//     : false
+// });
+
+
+// pool.on("connect", () => {
+//   console.log("PostgreSQL conectado");
+// });
+
+// pool.on("error", (err) => {
+//   console.error("Error inesperado en PostgreSQL", err);
+// });
