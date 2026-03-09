d7dc09df2d0bff96f33cb63bd13e17dbb1bcf31d
Pabloc <pabloc12@gmail.com>
2026-03-08 21:51:05 -0300

    fix postgres ssl

--- backend/db/postgres.js
+++ backend/db/postgres.js
@@ -3,14 +3,31 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+// export const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+
+//   ssl: process.env.NODE_ENV === "production"
+//     ? { rejectUnauthorized: false }
+//     : false
+// });
+
+
+// pool.on("connect", () => {
+//   console.log("PostgreSQL conectado");
+// });
+
+// pool.on("error", (err) => {
+//   console.error("Error inesperado en PostgreSQL", err);
+// });
+
+import { Pool } from "pg";
+
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-
-  ssl: process.env.NODE_ENV === "production"
-    ? { rejectUnauthorized: false }
-    : false
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
-
 
 pool.on("connect", () => {
   console.log("PostgreSQL conectado");
