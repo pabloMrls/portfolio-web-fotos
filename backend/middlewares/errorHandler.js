0c2354703d3bdda86dc4e30cb94f172bb8ae0e6d
Pabloc <pabloc12@gmail.com>
2026-03-08 21:37:14 -0300

    debug error handler

--- backend/middlewares/errorHandler.js
+++ backend/middlewares/errorHandler.js
@@ -1,15 +1,24 @@
+// export function errorHandler(err, req, res, next) {
+
+//   const status = err.status || 500;
+//   const isProduction = process.env.NODE_ENV === "production";
+
+//   if (!isProduction) {
+//     console.error(err);
+//   }
+
+//   res.status(status).json({
+//     error: isProduction && status === 500
+//       ? "Error interno del servidor"
+//       : err.message
+//   });
+// }
+
 export function errorHandler(err, req, res, next) {
 
-  const status = err.status || 500;
-  const isProduction = process.env.NODE_ENV === "production";
+  console.error("ERROR REAL:", err);
 
-  if (!isProduction) {
-    console.error(err);
-  }
-
-  res.status(status).json({
-    error: isProduction && status === 500
-      ? "Error interno del servidor"
-      : err.message
+  res.status(err.status || 500).json({
+    error: err.message
   });
 }
